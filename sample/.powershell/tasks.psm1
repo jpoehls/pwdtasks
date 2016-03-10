b2155f6214985350c31bd1d94c45dfe19679aade
Joshua Poehls <joshua@poehls.me>
2016-03-10 06:13:57 -0600

    Added note to sample tasks.psm1 about depending on pwdtasks

--- sample/.powershell/tasks.psm1
+++ sample/.powershell/tasks.psm1
@@ -1,3 +1,7 @@
+# Import-Module https://github.com/jpoehls/pwdtasks to have
+# this tasks.psm1 module automatically loaded whenever you
+# `cd` into your project's directory tree.
+
 Set-StrictMode -Version Latest
 
 # Grab the path to your project's root directory.
